a0b0b793ea5c6dc981b26abb1ac9732ffbb5d982
storytellerF <34095089+storytellerF@users.noreply.github.com>
2024-09-12 22:47:04 +0800

    new Dockerfile.koyeb

--- scripts/build-mini-service.sh
+++ scripts/build-mini-service.sh
@@ -1,4 +1,4 @@
-sh gradlew server:buildFatJar
+sh build-server.sh
 cp ./server/build/libs/*-all.jar ./deploy
 cd deploy
 COMPOSE_PROFILES=db,media,web docker compose --env-file ../mini.env up --build
---
+++ scripts/build-server.sh
@@ -0,0 +1,1 @@
+sh gradlew server:buildFatJar
