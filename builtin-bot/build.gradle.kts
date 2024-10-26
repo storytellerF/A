c7878f8f47ddb0f80368b4f30d5e82de4829a1eb
storytellerF <34095089+storytellerF@users.noreply.github.com>
2024-10-26 16:55:13 +0800

    [no ci]plugin version

--- builtin-bot/build.gradle.kts
+++ builtin-bot/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.ktor)
     application
-    id("com.github.gmazzo.buildconfig") version "5.4.0"
+    alias(libs.plugins.kotlinBuildConfig)
 }
 
 group = "com.storyteller_f.a"
