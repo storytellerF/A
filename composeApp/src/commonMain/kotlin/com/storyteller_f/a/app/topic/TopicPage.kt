5a145581f92acaf923a88caf642a4d0d45307018
storytellerF <34095089+storytellerF@users.noreply.github.com>
2024-12-07 16:24:52 +0800

    fix: emoji animation recomposing issue

--- composeApp/src/commonMain/kotlin/com/storyteller_f/a/app/topic/TopicPage.kt
+++ composeApp/src/commonMain/kotlin/com/storyteller_f/a/app/topic/TopicPage.kt
@@ -10,12 +10,14 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Topic
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewModelScope
@@ -52,7 +54,6 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.jetbrains.compose.resources.getString
 import org.kodein.emoji.Emoji
-import org.kodein.emoji.compose.m3.TextWithNotoAnimatedEmoji
 import org.kodein.emoji.list
 import kotlin.time.Duration.Companion.seconds
 
@@ -96,41 +97,102 @@ fun EmojiPicker(
     topic: TopicInfo,
     hideSheet: () -> Unit
 ) {
-    val scope = rememberCoroutineScope()
+    var query by remember {
+        mutableStateOf("")
+    }
+
     if (showSheet) {
         ModalBottomSheet(
-            modifier = Modifier.navigationBarsPadding(),
             onDismissRequest = {
                 hideSheet()
             },
-            sheetState = sheetState
+            dragHandle = null,
+            sheetState = sheetState,
+            contentWindowInsets = {
+                WindowInsets(0)
+            },
         ) {
-            LazyVerticalGrid(
-                GridCells.FixedSize(40.dp),
-                contentPadding = PaddingValues(20.dp),
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            ) {
-                items(Emoji.list(), key = {
-                    it.toString()
-                }) {
-                    Box(modifier = Modifier.size(40.dp).clickable {
-                        scope.launch {
-                            globalDialogState.use {
-                                client.addReaction(topic.id, it.details.string)
-                                bus.emit(OnTopicChanged(topic.copy(reactionCount = topic.reactionCount + 1)))
-                                sheetState.hide()
-                            }
-                        }.invokeOnCompletion {
-                            if (!sheetState.isVisible) {
-                                hideSheet()
-                            }
-                        }
-                    }, contentAlignment = Alignment.Center) {
-                        TextWithNotoAnimatedEmoji(it.details.string, fontSize = 25.sp)
-                    }
+            EmojiPickerInternal(query, topic, sheetState, hideSheet) {
+                query = it
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun EmojiPickerInternal(
+    query: String,
+    topic: TopicInfo,
+    sheetState: SheetState,
+    hideSheet: () -> Unit,
+    updateQuery: (String) -> Unit,
+) {
+    Column(modifier = Modifier.fillMaxWidth().consumeWindowInsets(WindowInsets.navigationBars)) {
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(query, {
+            updateQuery(it)
+        }, suffix = {
+            Icon(Icons.Default.Clear, "clear reaction query")
+        }, modifier = Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.height(10.dp))
+        val emojiList by produceState(emptyList<Emoji>(), query) {
+            value = if (query.isEmpty()) {
+                Emoji.list()
+            } else {
+                Emoji.list().filter { emoji ->
+                    emoji.details.description.contains(query, true)
                 }
             }
         }
+        val emojiSize = 50.dp
+        BoxWithConstraints(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+            val contentWidth = maxWidth - 40.dp
+            val count = (contentWidth / emojiSize).toInt()
+            val style = if (emojiSize * count == contentWidth) {
+                GridCells.FixedSize(emojiSize)
+            } else {
+                GridCells.Fixed(count)
+            }
+            LazyVerticalGrid(
+                style,
+                contentPadding = PaddingValues(20.dp, 10.dp),
+                modifier = Modifier.wrapContentWidth().height(300.dp)
+            ) {
+                items(emojiList, key = {
+                    it.toString()
+                }) {
+                    EmojiItem(emojiSize, topic, it, sheetState, hideSheet)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun EmojiItem(
+    emojiSize: Dp,
+    topic: TopicInfo,
+    emoji: Emoji,
+    sheetState: SheetState,
+    hideSheet: () -> Unit
+) {
+    val scope = rememberCoroutineScope()
+    Box(modifier = Modifier.size(emojiSize).clickable {
+        scope.launch {
+            globalDialogState.use {
+                client.addReaction(topic.id, emoji.details.string)
+                bus.emit(OnTopicChanged(topic.copy(reactionCount = topic.reactionCount + 1)))
+                sheetState.hide()
+            }
+        }.invokeOnCompletion {
+            if (!sheetState.isVisible) {
+                hideSheet()
+            }
+        }
+    }, contentAlignment = Alignment.Center) {
+        Text(emoji.details.string, fontSize = 25.sp)
     }
 }
 
