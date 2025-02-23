a1a25491f30c61861803c96751da57a098f8af56
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-02-23 23:10:50 +0800

    fix desktop blur in dev
    Former-commit-id: c74b6ac988cd21667dcab5859d3997d6b69fd14d

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -5,8 +5,19 @@ import com.kdroid.composenotification.builder.NotificationInitializer
 import com.storyteller_f.a.app.App
 import com.storyteller_f.a.app.utils.restoreFromStorage
 import com.storyteller_f.crypto_jvm.addProviderForJvm
+import java.awt.Toolkit
+import javax.swing.UIManager
 
 fun main() {
+    if (!com.storyteller_f.a.app.AppConfig.IS_PROD) {
+        val dpi: Int = Toolkit.getDefaultToolkit().screenResolution
+        println("Screen DPI: $dpi")
+        System.setProperty("sun.java2d.uiScale.enabled", "true")
+        System.setProperty("sun.java2d.uiScale", "${dpi / 100}")
+        UIManager.put("swing.boldMetal", "false")
+        System.setProperty("awt.useSystemAAFontSettings", "on")
+    }
+
     NotificationInitializer.configure(
         AppConfig(
             appName = "My awesome app",
