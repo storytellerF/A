690e296d5f910c20e82a9efb91837cd129aacf84
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-05-31 12:36:30 +0800

    fix: image dimension
    Former-commit-id: 0d05aca040364a008b33961639f126a6de30f138

--- server/src/test/kotlin/MediaTest.kt
+++ server/src/test/kotlin/MediaTest.kt
@@ -1,14 +1,18 @@
 import com.storyteller_f.a.client_lib.copy
 import com.storyteller_f.a.client_lib.getMediaList
 import com.storyteller_f.a.client_lib.upload
+import com.storyteller_f.media.getImageDimension
 import com.storyteller_f.shared.obj.ObjectTuple
 import com.storyteller_f.shared.obj.ob
 import com.storyteller_f.shared.type.ObjectType
 import io.ktor.http.*
+import kotlinx.coroutines.test.runTest
 import kotlinx.io.Buffer
 import kotlinx.io.writeString
+import javax.imageio.ImageIO
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class MediaTest {
     @Test
@@ -34,6 +38,21 @@ class MediaTest {
                 assertEquals("${it.uid}/hello.txt", response.data.first().name)
                 assertListSize(1, getMediaList(it.uid, ObjectType.USER, null, 10))
             }
+        }
+    }
+
+    @Test
+    fun `get png size`() {
+        runTest {
+            val bufferedImage = ImageIO.read(ClassLoader.getSystemClassLoader().getResourceAsStream("avatar1.png"))
+            assertEquals(bufferedImage.width, 420)
+            assertEquals(bufferedImage.height, 420)
+            val dimension = getImageDimension("avatar1.png", "image/png") {
+                ClassLoader.getSystemClassLoader().getResourceAsStream("avatar1.png")!!
+            }
+            assertNotNull(dimension)
+            assertEquals(dimension.width, 420)
+            assertEquals(dimension.height, 420)
         }
     }
 }
