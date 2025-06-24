5a665c98e922c4965fd2ab76ee31a54551b4b797
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-06-24 17:19:27 +0800

    auto create snapshot key store

--- scripts/tool_scripts/save-env.sh
+++ scripts/tool_scripts/save-env.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 OUTPUT_FILE="$FLAVOR.env"
 
-# 定义 env-filter 文件名
-env_filter_file="env-filter"
+truncate -s 0 "$OUTPUT_FILE"
 
-# 创建或清空 .env 文件
-> $OUTPUT_FILE
-
-# 遍历 env-filter 文件中的每一行
 while IFS= read -r key; do
   # 去除可能的 \r 符号（处理 Windows 换行符）
   key=$(echo "$key" | tr -d '\r')
@@ -16,7 +11,7 @@ while IFS= read -r key; do
   # 获取对应的环境变量值 bash 特有
   value="${!key}"
   # 对反斜杠进行转义
-  value=$(echo "$value" | sed 's/\\/\\\\/g')
+  value="${value//\\/\\\\}"
 
   # 如果值中包含空格，则用引号包裹
   if echo "$value" | grep -q ' '; then
@@ -24,6 +19,6 @@ while IFS= read -r key; do
   else
       echo "$upper_key=$value" >> "$OUTPUT_FILE"
   fi
-done < "$env_filter_file"
+done < "env-filter"
 
 echo "$FLAVOR.env 文件已生成。"
