134960a231ec2b30f06753814f6f33b2d315cecf
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-07-16 11:37:44 +0800

    use --no-daemon in build-app-on-ci

--- scripts/build_scripts/build-app-on-ci.sh
+++ scripts/build_scripts/build-app-on-ci.sh
@@ -44,25 +44,25 @@ case "$TARGET" in
     android)
         echo "Running Android-specific command..."
         # 在这里添加 Android 相关命令
-        ./gradlew app:composeApp:assembleRelease
+        ./gradlew app:composeApp:assembleRelease --no-daemon
         mkdir -p "build/outputs/apk/release"
         mv app/composeApp/build/outputs/apk/release/*.apk "build/outputs/apk/release/$FLAVOR.apk"
         ;;
     desktop-msi)
         echo "Running DesktopMsi-specific command..."
-        ./gradlew app:composeApp:packageReleaseMsi
+        ./gradlew app:composeApp:packageReleaseMsi --no-daemon
         mkdir -p "build/outputs/pkg/release"
         mv app/composeApp/build/compose/binaries/main-release/msi/*.msi "build/outputs/pkg/release/$FLAVOR.msi"
         ;;
     desktop-deb)
         echo "Running DesktopDeb-specific command..."
-        ./gradlew app:composeApp:packageReleaseDeb
+        ./gradlew app:composeApp:packageReleaseDeb --no-daemon
         mkdir -p "build/outputs/pkg/release"
         mv app/composeApp/build/compose/binaries/main-release/deb/*.deb "build/outputs/pkg/release/$FLAVOR.deb"
         ;;
     desktop-dmg)
         echo "Running DesktopDmg-specific command..."
-        ./gradlew app:composeApp:packageReleaseDmg
+        ./gradlew app:composeApp:packageReleaseDmg --no-daemon
         mkdir -p "build/outputs/pkg/release"
         mv app/composeApp/build/compose/binaries/main-release/dmg/*.dmg "build/outputs/pkg/release/$FLAVOR.dmg"
         ;;
