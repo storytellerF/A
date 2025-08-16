8f14d3bb609434b2e12e87c3bad7f5196e3b2b93
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-08-16 19:25:47 +0800

    fix: specify bot env file

--- scripts/service_scripts/start-bot-in-local.sh
+++ scripts/service_scripts/start-bot-in-local.sh
@@ -10,4 +10,4 @@ echo "build on local"
 # 在本地构建，本地启动
 ./scripts/build_scripts/build-bot.sh
 docker build --build-arg FLAVOR="$FLAVOR" --build-arg BUILD_ON=local --build-arg BUILD_TYPE=prod -t bot:latest -f bot.Dockerfile .
-docker run -itd bot:latest
+docker run -itd --env-file "./$FLAVOR.env" bot:latest
