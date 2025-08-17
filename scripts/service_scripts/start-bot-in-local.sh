135f6cc5e8a77a1fce24b252d638c5b87159898a
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-08-17 21:01:54 +0800

    fix: safe stop bot container

--- scripts/service_scripts/start-bot-in-local.sh
+++ scripts/service_scripts/start-bot-in-local.sh
@@ -10,5 +10,11 @@ echo "build on local"
 # 在本地构建，本地启动
 ./scripts/build_scripts/build-bot.sh
 docker build --build-arg FLAVOR="$FLAVOR" --build-arg BUILD_ON=local --build-arg BUILD_TYPE=prod -t bot:latest -f bot.Dockerfile .
-docker stop "$(docker ps -a -q --filter name="bot")"
-docker run -itd --env-file "./$FLAVOR.env" -t bot bot:latest
+ID=$(docker ps -a -q --filter name="bot")
+if [ -n "$ID" ]; then
+    docker stop $ID
+else
+    echo "没有找到 bot 容器"
+fi
+
+docker run -itd --env-file "./$FLAVOR.env" --name bot bot:latest
