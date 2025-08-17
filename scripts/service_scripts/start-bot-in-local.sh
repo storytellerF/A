8bef32639ba548e2fb715aa66608f95fce8b7600
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-08-17 10:28:56 +0800

    fix: bot create new topic date check

--- scripts/service_scripts/start-bot-in-local.sh
+++ scripts/service_scripts/start-bot-in-local.sh
@@ -10,4 +10,5 @@ echo "build on local"
 # 在本地构建，本地启动
 ./scripts/build_scripts/build-bot.sh
 docker build --build-arg FLAVOR="$FLAVOR" --build-arg BUILD_ON=local --build-arg BUILD_TYPE=prod -t bot:latest -f bot.Dockerfile .
-docker run -itd --env-file "./$FLAVOR.env" bot:latest
+docker stop "$(docker ps -a -q --filter name="bot")"
+docker run -itd --env-file "./$FLAVOR.env" -t bot bot:latest
