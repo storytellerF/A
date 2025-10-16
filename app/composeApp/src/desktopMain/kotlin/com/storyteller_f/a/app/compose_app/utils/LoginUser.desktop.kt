ba28021d5fcf8d1ff00916850f08d472176bd52a
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-10-16 15:18:40 +0800

    remove Settings.kt

--- app/composeApp/src/desktopMain/kotlin/com/storyteller_f/a/app/compose_app/utils/LoginUser.desktop.kt
+++ app/composeApp/src/desktopMain/kotlin/com/storyteller_f/a/app/compose_app/utils/LoginUser.desktop.kt
@@ -1,8 +1,13 @@
 package com.storyteller_f.a.app.compose_app.utils
 
+import com.russhwolf.settings.PreferencesSettings
 import com.russhwolf.settings.Settings
 
 actual fun buildLoginHistoryFactory(settings: Settings): LoginHistoryManager {
     return DefaultLoginHistoryManager(settings)
+}
+
+actual fun createSettings(name: String): Settings {
+    return PreferencesSettings.Factory().create(name)
 }
 
