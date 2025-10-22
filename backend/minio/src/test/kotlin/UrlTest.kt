28b2a5e81381cf4bad65ac13de2151da5fef9d01
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-10-22 09:25:19 +0800

    fix: url replace port issue

--- backend/minio/src/test/kotlin/UrlTest.kt
+++ backend/minio/src/test/kotlin/UrlTest.kt
@@ -1,5 +1,6 @@
 import com.storyteller_f.a.backend.minio.replaceUrl
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class UrlTest {
     @Test
@@ -7,6 +8,14 @@ class UrlTest {
         val host = "https://test.com:9090"
         val url = "http://test.com:909/test?a=1&b=2"
         val newUrl = replaceUrl(host, url)
-        assert(newUrl == "https://test.com:9090/test?a=1&b=2")
+        assertEquals("https://test.com:9090/test?a=1&b=2", newUrl)
+    }
+
+    @Test
+    fun `test url replace 80`() {
+        val host = "https://test.com"
+        val url = "http://test.com:909/test?a=1&b=2"
+        val newUrl = replaceUrl(host, url)
+        assertEquals("https://test.com:80/test?a=1&b=2", newUrl)
     }
 }
