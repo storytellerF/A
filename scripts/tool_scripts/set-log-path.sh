95d8f0ac789721b0b001e3ab55e0fe60da3a4c90
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-11-03 20:29:36 +0800

    add set-log-path.sh

---
+++ scripts/tool_scripts/set-log-path.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# 获取操作系统类型
+OS_NAME=$(uname | tr '[:upper:]' '[:lower:]')
+
+if [[ "$OS_NAME" == "linux" || "$OS_NAME" == "darwin" ]]; then
+    LOG_PATH="/var/logs"
+elif [[ "$OS_NAME" == *"mingw"* || "$OS_NAME" == *"cygwin"* ]]; then
+    # Windows 下 bash (Git Bash / Cygwin)
+    LOG_PATH=$(java -XshowSettings:properties -version 2>&1 | grep 'java.io.tmpdir' | awk -F= '{gsub(/ /,"",$2); print $2}')
+else
+    # 默认使用 tmp
+    LOG_PATH=$(java -XshowSettings:properties -version 2>&1 | grep 'java.io.tmpdir' | awk -F= '{gsub(/ /,"",$2); print $2}')
+fi
+
+export LOG_PATH
+echo "LOG_PATH set to: $LOG_PATH"
