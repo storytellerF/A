df58d8ce11daa3d27b36fa94f6d0f8836b455094
storytellerF <34095089+storytellerF@users.noreply.github.com>
2026-02-01 15:59:32 +0800

    fix: ascii-parser test

--- client/ascii-parser/build.gradle.kts
+++ client/ascii-parser/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
@@ -19,9 +21,22 @@ kotlin {
         }
     }
 
-    androidTarget {
+    android {
+        namespace = "com.storyteller_f.a.client.ascii_parser"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_21)
+        }
+        withHostTest {
+            isIncludeAndroidResources = true
+        }
+        withDeviceTest { }
+        optimization {
+            consumerKeepRules.apply {
+                publish = true
+                file("consumer-rules.pro")
+            }
         }
     }
 
@@ -48,22 +63,28 @@ kotlin {
         commonMain.dependencies {
             compileOnly(libs.javet)
         }
-        androidUnitTest.dependencies {
-            implementation(libs.robolectric)
+        getByName("androidHostTest") {
+            dependencies {
+                implementation(libs.robolectric)
 
-            implementation(libs.javet.node.linux.arm64)
-            implementation(libs.javet.node.linux.x86.x4)
-            implementation(libs.javet.node.macos.arm64)
-            implementation(libs.javet.node.macos.x86.x4)
-            implementation(libs.javet.node.windows.x86.x4)
-        }
-        androidUnitTest {
+                implementation(libs.javet.node.linux.arm64)
+                implementation(libs.javet.node.linux.x86.x4)
+                implementation(libs.javet.node.macos.arm64)
+                implementation(libs.javet.node.macos.x86.x4)
+                implementation(libs.javet.node.windows.x86.x4)
+            }
             dependsOn(headlessTest)
+        }
+        getByName("androidDeviceTest") {
+            dependencies {
+                implementation(libs.androidx.ui.test.junit4.android)
+            }
         }
         commonTest.dependencies {
             implementation(kotlin("test"))
             implementation(libs.kotlinx.coroutines.test)
         }
+
         jvmMain.dependencies {
             implementation(libs.javet)
             implementation(libs.javet.node.linux.arm64)
@@ -72,15 +93,11 @@ kotlin {
             implementation(libs.javet.node.macos.x86.x4)
             implementation(libs.javet.node.windows.x86.x4)
         }
-        jvmMain {
-        }
         jvmTest {
             dependsOn(headlessTest)
         }
         if (buildIosTarget) {
             iosMain.dependencies {
-            }
-            iosMain {
             }
         }
     }
@@ -90,30 +107,5 @@ kotlin {
 }
 
 dependencies {
-    androidTestImplementation(libs.androidx.ui.test.junit4.android)
-    debugImplementation(libs.androidx.ui.test.manifest)
-}
-
-android {
-    namespace = "com.storyteller_f.a.client.kotbase"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    compileOptions {
-        val javaVersion = JavaVersion.forClassVersion(libs.versions.jdk.get().toInt())
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-    }
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
-    testOptions {
-        unitTests.all {
-            val dir = project.layout.buildDirectory.dir("native-libs/couchbase").get().asFile
-            it.jvmArgs(
-                "-Djava.library.path=$dir"
-            )
-        }
-        unitTests {
-            isIncludeAndroidResources = true
-        }
-    }
+    androidRuntimeClasspath(libs.androidx.ui.test.manifest)
 }
