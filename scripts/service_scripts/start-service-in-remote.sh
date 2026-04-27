76cf988bafd27646b1e408cd3b971c463eda2e69
storytellerF <34095089+storytellerF@users.noreply.github.com>
2026-04-27 23:07:27 +0800

    add log

--- scripts/service_scripts/start-service-in-remote.sh
+++ scripts/service_scripts/start-service-in-remote.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. ./scripts/tool_scripts/terminal-log.sh
+
 FLAVOR=$1
 
 if [ -z "$FLAVOR" ]; then
@@ -7,15 +9,18 @@ if [ -z "$FLAVOR" ]; then
   exit 1
 fi
 
+log "Current flavor is $FLAVOR"
+
 SESSION_NAME="A"
 
 # 检查是否存在，存在则删除
 tmux has-session -t "$SESSION_NAME" 2>/dev/null
 if [ $? -eq 0 ]; then
-  echo "发现旧会话 $SESSION_NAME, 正在关闭..."
+  log "发现旧会话 $SESSION_NAME, 正在关闭..."
   tmux kill-session -t "$SESSION_NAME"
 else
-  echo "没有会话"
+  log "没有会话"
 fi
 
+log "Start service..."
 tmux new-session -d -s $SESSION_NAME "./scripts/service_scripts/start-service-in-local.sh $FLAVOR; exec bash"
