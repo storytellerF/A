8b99406eb69f5787a7748b9c00204f92f3e47d09
storytellerF <34095089+storytellerF@users.noreply.github.com>
2026-06-07 16:31:58 +0800

    Serialize background login requests

--- client/core/src/commonMain/kotlin/com/storyteller_f/a/client/core/SessionManager.kt
+++ client/core/src/commonMain/kotlin/com/storyteller_f/a/client/core/SessionManager.kt
@@ -19,10 +19,8 @@ import io.ktor.client.plugins.cookies.AcceptAllCookiesStorage
 import io.ktor.client.plugins.cookies.CookiesStorage
 import io.ktor.client.plugins.websocket.DefaultClientWebSocketSession
 import io.ktor.client.plugins.websocket.webSocketSession
-import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.withContext
 import kotlin.time.ExperimentalTime
 
 interface SessionModel<U> {
@@ -169,17 +167,15 @@ suspend fun UserSessionManager.login() {
         userHandler.done(it)
     }) {
         runCatching {
-            withContext(NonCancellable) {
-                val data = getData().getOrThrow()
-                val address = userPass.address().getOrThrow()
-                val signature = userPass.signature(finalData(data)).getOrThrow()
-                val userInfo = when (val response = signIn(SignInBody(address, signature)).getOrThrow()) {
-                    is SignInResponse.Success -> response.userInfo
-                    SignInResponse.RequiresTotp -> error("totp required")
-                }
-                model.updateSignature(data, signature)
-                userInfo
+            val data = getData().getOrThrow()
+            val address = userPass.address().getOrThrow()
+            val signature = userPass.signature(finalData(data)).getOrThrow()
+            val userInfo = when (val response = signIn(SignInBody(address, signature)).getOrThrow()) {
+                is SignInResponse.Success -> response.userInfo
+                SignInResponse.RequiresTotp -> error("totp required")
             }
+            model.updateSignature(data, signature)
+            userInfo
         }
     }
 }
@@ -191,14 +187,12 @@ suspend fun PanelSessionManager.login() {
         userHandler.done(it)
     }) {
         runCatching {
-            withContext(NonCancellable) {
-                val data = getData().getOrThrow()
-                val address = userPass.address().getOrThrow()
-                val signature = userPass.signature(finalData(data)).getOrThrow()
-                val userInfo = signIn(SignInBody(address, signature)).getOrThrow()
-                model.updateSignature(data, signature)
-                userInfo
-            }
+            val data = getData().getOrThrow()
+            val address = userPass.address().getOrThrow()
+            val signature = userPass.signature(finalData(data)).getOrThrow()
+            val userInfo = signIn(SignInBody(address, signature)).getOrThrow()
+            model.updateSignature(data, signature)
+            userInfo
         }
     }
 }
--- client/core/src/commonMain/kotlin/com/storyteller_f/a/client/core/SessionManagerKt.kt
+++ client/core/src/commonMain/kotlin/com/storyteller_f/a/client/core/SessionManagerKt.kt
@@ -5,6 +5,7 @@ package com.storyteller_f.a.client.core
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
@@ -14,8 +15,16 @@ import io.github.aakira.napier.Napier
 
 context(c: CoroutineScope)
 fun UserSessionManager.startBackgroundTask(): List<Job> {
-    return listOf(c.launch {
-        val model = model
+    val model = model
+    val loginRequests = Channel<Unit>(Channel.CONFLATED)
+    val loginWorker = c.launch {
+        for (ignored in loginRequests) {
+            if (model.state.value is ClientSessionState.Success && model.userHandler.data.value == null) {
+                login()
+            }
+        }
+    }
+    val loginObserver = c.launch {
         combine(model.state, model.userHandler.data) { t1, t2 ->
             t1 to t2
         }.distinctUntilChanged().collect { (state, userInfo) ->
@@ -23,20 +32,23 @@ fun UserSessionManager.startBackgroundTask(): List<Job> {
                 "background task state=${state::class.simpleName} hasUser=${userInfo != null}"
             }
             if (state is ClientSessionState.Success && userInfo == null) {
-                login()
+                loginRequests.trySend(Unit)
             }
         }
-    }, c.launch {
+    }
+    val addressUpdater = c.launch {
         model.state.collect {
             updateAddress(it)
         }
-    }, c.launch {
+    }
+    val webSocketConnector = c.launch {
         webSocketClient.connectWebSocket()
-    })
+    }
+    return listOf(loginObserver, loginWorker, addressUpdater, webSocketConnector)
 }
 
 context(c: CoroutineScope)
-suspend inline fun <R> UserSessionManager.startBackgroundTask(block: UserSessionManager.() -> R): R {
+suspend inline fun <R> UserSessionManager.onBackgroundTask(block: UserSessionManager.() -> R): R {
     val jobs = startBackgroundTask()
     val result = block()
     jobs.forEach {
@@ -47,24 +59,34 @@ suspend inline fun <R> UserSessionManager.startBackgroundTask(block: UserSession
 
 context(c: CoroutineScope)
 fun PanelSessionManager.startBackgroundTask(): List<Job> {
-    return listOf(c.launch {
-        val model = model
+    val model = model
+    val loginRequests = Channel<Unit>(Channel.CONFLATED)
+    val loginWorker = c.launch {
+        for (ignored in loginRequests) {
+            if (model.state.value is ClientSessionState.Success && model.userHandler.data.value == null) {
+                login()
+            }
+        }
+    }
+    val loginObserver = c.launch {
         combine(model.state, model.userHandler.data) { t1, t2 ->
             t1 to t2
         }.distinctUntilChanged().collect { (state, userInfo) ->
             if (state is ClientSessionState.Success && userInfo == null) {
-                login()
+                loginRequests.trySend(Unit)
             }
         }
-    }, c.launch {
+    }
+    val addressUpdater = c.launch {
         model.state.collect {
             updateAddress(it)
         }
-    })
+    }
+    return listOf(loginObserver, loginWorker, addressUpdater)
 }
 
 context(c: CoroutineScope)
-suspend inline fun <R> PanelSessionManager.startBackgroundTask(block: PanelSessionManager.() -> R): R {
+suspend inline fun <R> PanelSessionManager.onBackgroundTask(block: PanelSessionManager.() -> R): R {
     val jobs = startBackgroundTask()
     val result = block()
     jobs.forEach {
