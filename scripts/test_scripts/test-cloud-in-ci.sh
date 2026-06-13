39fdbc20aa87d76c64b42369d5d403f8b5917922
storytellerF <34095089+storytellerF@users.noreply.github.com>
2026-06-13 21:50:48 +0800

    Fix cloud CI test credentials

---
+++ scripts/test_scripts/test-cloud-in-ci.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ~/.gradle
+echo "gpr.user=$ORG_GRADLE_PROJECT_gpr_user" >> ~/.gradle/gradle.properties
+echo "gpr.key=$ORG_GRADLE_PROJECT_gpr_key" >> ~/.gradle/gradle.properties
+
+ENABLE_TEST_CONTAINER=true ./gradlew :backend:minio:test :cloud:cli:test :cloud:service:test :cloud:server:test --console=plain
