1ff8a72ff3a931a82bcb0ce50ce60d1de8090929
AgentStart1 <295559726+AgentStart1@users.noreply.github.com>
2026-07-30 23:27:12 +0800

    client/core: include credentials for Wasm API requests

--- client/core/src/wasmJsMain/kotlin/com/storyteller_f/a/client/core/HttpClient.wasmJs.kt
+++ client/core/src/wasmJsMain/kotlin/com/storyteller_f/a/client/core/HttpClient.wasmJs.kt
@@ -1,9 +1,30 @@
+@file:OptIn(ExperimentalWasmJsInterop::class)
+
 package com.storyteller_f.a.client.core
 
 import io.ktor.client.*
+import io.ktor.client.engine.js.Js
+import io.ktor.client.fetchOptions
+import io.ktor.client.plugins.api.createClientPlugin
+import io.ktor.http.protocolWithAuthority
+import kotlin.js.ExperimentalWasmJsInterop
+import kotlin.js.toJsString
 
-actual fun getClient(block: HttpClientConfig<*>.() -> Unit): HttpClient {
-    return HttpClient {
-        block()
+private val credentialedApiRequests =
+    createClientPlugin("CredentialedApiRequests") {
+        onRequest { request, _ ->
+            val credentialedOrigin = request.attributes.getOrNull(CredentialedOriginAttribute)
+            if (credentialedOrigin != null && request.url.build().protocolWithAuthority == credentialedOrigin) {
+                request.fetchOptions {
+                    credentials = "include".toJsString()
+                }
+            }
+        }
     }
+
+private fun HttpClientConfig<*>.configureClient(block: HttpClientConfig<*>.() -> Unit) {
+    install(credentialedApiRequests)
+    block()
 }
+
+actual fun getClient(block: HttpClientConfig<*>.() -> Unit): HttpClient = HttpClient(Js) { configureClient(block) }
